dad40efc2f5668c367bf95e706bac0c4e58b0015
Skylar Rearick <rearick.skylar@gmail.com>
2023-11-05 23:46:04 -0500

    clean up and added sample components

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,100 +1,72 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import React, { Component } from "react";
+import "./App.css";
+import Results from "./components/Results";
+import NewName from "./components/NewName";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Basic Example</h1>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      names: [],
+      loading: true,
+    };
 
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
 
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    await fetch("/addname/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+      }),
+    });
+    this.getNames();
+  }
+
+  getNames() {
+    fetch("/getnames/")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          name: "",
+          names: json,
+          loading: false,
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.getNames();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NewName
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            value={this.state.name}
+          />
+          {this.state.loading ? <h1>Loading</h1> : <Results {...this.state} />}
+        </header>
+      </div>
+    );
+  }
 }
 
-function Layout() {
-  return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
+export default App;
